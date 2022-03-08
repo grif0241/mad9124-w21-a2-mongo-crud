bc40a2e3caa6df29e8a20f60f865b0b05231d474
grif0241 <grif0241@algonquinlive.com>
2022-03-08 00:15:37 -0500

    Add resource not found function

--- routes/students.js
+++ routes/students.js
@@ -17,12 +17,35 @@ router.post('/', async (req, res) => {
   res.status(201).send({ data: newStudent})
 })
 
-router.get('/:id', async (req, res) => {})
+router.get('/:id', async (req, res) => {
+  try {
+    const student = await Student.findById(req.params.id)
+    if (!car) {
+      throw new Error ('Resource not found')
+    }
+    res.send({data: student})
+  } catch(err) {
+    sendResourceNotFound(req, res)
+  }
+})
 
 router.patch('/:id', async (req, res) => {})
 
 router.put('/:id', async (req, res) => {})
 
 router.delete('/:id', async (req, res) => {})
+
+
+function sendResourceNotFound(req, res) {
+  res.status(404).send({
+    errors: [
+      {
+        status: '404',
+        title: 'Resource does not exist',
+        description: `We could not find a student with the id ${req.params.id}`
+      }
+    ]
+  })
+}
 
 module.exports = router
