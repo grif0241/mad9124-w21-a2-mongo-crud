ad5d62fc9bfab1f6427824d8e98ee8100d81d46a
grif0241 <grif0241@algonquinlive.com>
2022-03-07 23:49:55 -0500

    Add get all students route logic

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const mongoose = require('mongoose')
 mongoose
   .connect('mongodb://localhost:27017/mad9124', {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  // useUnifiedTopology: true
 })
 .then(() => {
-  console.log("Connect to MongoDB...")
+  console.log("Connected to MongoDB...")
 })
 .catch((err) => {
   console.log("Problem connecting with MongoDB...", err.message)
@@ -22,6 +22,7 @@ const app = express()
 
 app.use(morgan('tiny'))
 app.use(express.json())
+app.use('/api/students', require('./routes/students'))
 
 const port = process.env.PORT || 3030
 app.listen(port, () => {
